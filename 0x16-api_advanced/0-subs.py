fd8f25e1b63ddc46a7e8473c57532e723065e5ce
Senyo197 <Senyo197@gmail.com>
2024-02-13 21:43:28 +0000

    A unction that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit,
+             or 0 if the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64)\
+               AppleWebKit/537.36"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 404:
+        return 0
+
+    try:
+        data = response.json().get("data")
+        subscribers = data.get("subscribers")
+        return subscribers
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        None: If the subreddit is not found or if there are no posts available.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) \
+               AppleWebKit/537.36"}
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 404:
+        print("None")
+        return
+
+    data = response.json().get("data", {}).get("children", [])
+    if not data:
+        print("None")
+        return
+
+    for post in data:
+        print(post["data"]["title"])
